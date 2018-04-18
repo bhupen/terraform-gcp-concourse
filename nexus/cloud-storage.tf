4134123291ca67eb58456422a8cf0fdef246fd09
Amy Breese <amy.breese@cna.com>
2018-04-18 12:47:27 -0500

    add blob-store

--- nexus/cloud-storage.tf
+++ nexus/cloud-storage.tf
@@ -1,4 +1,4 @@
-resource "google_storage_bucket" "keys-bucket" {
+resource "google_storage_bucket" "blob-store-bucket" {
   name          = "nexus-${random_id.cloud-storage-bucket.hex}"
   force_destroy = true
 }
